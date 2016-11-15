153ff4b0f7cb9baf45c87c3710f825f61f236740
Jirka Schäfer <jirka@tschitschereengreen.com>
2016-11-15 23:07:56 +0100

    fix path in deploy script
    backup needs to be with manage.py file

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,11 @@ source .ve/bin/activate
 git pull --no-edit && pip install -r requirements.txt | grep -v "Requirement already satisfied"
 
 echo "Creating fresh DB and media backups..."
-python ./site/manage.py dbbackup
-python ./site/manage.py mediabackup
+# needs to be run from site dir
+cd site
+./manage.py dbbackup
+./manage.py mediabackup
+cd ..
 
 python ./minify_static.py && ./site/manage.py migrate && ./site/manage.py collectstatic --noinput
 echo "touching to reload uwsgi..."
