1d61b3dd12e1efd38427c40e1d273dd7e5896abe
Jirka Schaefer <jirka.schaefer@kkd-partners.com>
2016-11-24 06:39:38 +0100

    add link to tos to signup form and provide translations

---
+++ site/project/forms.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from django.utils.translation import ugettext_lazy as _
+from django.utils.safestring import mark_safe
+
+from registration.forms import (RegistrationFormTermsOfService,
+                                RegistrationFormUniqueEmail)
+
+
+class RegistrationForm(RegistrationFormTermsOfService,
+                       RegistrationFormUniqueEmail):
+    """
+    merge forms to have unique email and ToS checkbox in registration
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        create form obj
+        """
+        res = super(RegistrationForm, self).__init__(*args, **kwargs)
+        # add url to ToS string in form
+        self.fields.get('tos').label = mark_safe(_(
+            'I have read and agree to the '
+            '<a href="http://www1.das-aktienregister.ch/agb" '
+            'alt="Darg Terms of Service" target="_blank" class="link tos">'
+            'Terms of Service'
+            '</a>'
+        ))
+        return res
