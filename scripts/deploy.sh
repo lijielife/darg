e0137cab03d5b1fdbc6ade86766c84666a3965dd
Jirka Schaefer <jirka.schaefer@kkd-partners.com>
2016-12-12 23:00:07 +0100

    moved all scripts into scripts dir to cleanup project

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ ! -d "scripts" ]; then
+  echo 'this script must be executed from project root'
+fi
 
 source .ve/bin/activate
 
@@ -11,7 +14,7 @@ cd site
 ./manage.py mediabackup
 cd ..
 
-python ./minify_static.py && ./site/manage.py migrate && ./site/manage.py collectstatic --noinput
+python ./scripts/minify_static.py && ./site/manage.py migrate && ./site/manage.py collectstatic --noinput
 echo "touching to reload uwsgi..."
 touch "/tmp/$USER-master.pid"
 echo "...done"
