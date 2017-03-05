adb45c8af1a91464c01b5d29145e21e6ca40a580
Jirka Schaefer <jirka.schaefer@kkd-partners.com>
2017-03-05 23:59:46 +0100

    updated version at base.py to 0.4.22

--- site/project/settings/base.py
+++ site/project/settings/base.py
@@ -30,7 +30,7 @@ def get_env_variable(var_name, fail_on_error=True):
     return env_var
 
 
-VERSION = '0.4.21'
+VERSION = '0.4.22'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
