1531bcf37c1df5a085de85b19f59450d93b56af3
Jirka Schaefer <jirka.schaefer@kkd-partners.com>
2017-03-31 20:37:03 +0200

    add full path to karma exec
    because its needed on CI

--- scripts/run_karma_tests.sh
+++ scripts/run_karma_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 echo 'run JS tests with karma...'
 cd darg.js
-karma start karma.ci.conf.js
+./node_modules/.bin/karma start karma.ci.conf.js
 cd ..
 echo 'JS tests finished'
