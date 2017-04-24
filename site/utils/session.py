3876cca2db7972d110985ae0d7abc86bb5ee61c2
Jirka Schaefer <jirka.schaefer@kkd-partners.com>
2017-04-24 09:51:53 +0200

    add fail_silently param to company_pk session helper and add helper to add company_pk to session for use in tests

--- site/utils/session.py
+++ site/utils/session.py
@@ -4,11 +4,20 @@
 from shareholder.models import Company
 
 
-def get_company_from_request(request):
+def get_company_from_request(request, fail_silently=False):
     """
     returns company obj based on session key
     """
-    if request.session.get('company_pk'):
+    if hasattr(request, 'session') and request.session.get('company_pk'):
         return Company.objects.get(pk=request.session.get('company_pk'))
 
+    if fail_silently:
+        return False
+
     raise ValueError('company_pk missing in session')
+
+
+def add_company_to_session(session, company):
+
+    session['company_pk'] = company.pk
+    session.save()
