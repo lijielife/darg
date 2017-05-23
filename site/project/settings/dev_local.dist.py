7a4e7bac126fa1573701a616721e498d97a0978c
Jirka Schaefer <jirka.schaefer@kkd-partners.com>
2017-05-23 21:12:40 +0200

    disable caching for tests

--- site/project/settings/dev_local.dist.py
+++ site/project/settings/dev_local.dist.py
@@ -9,3 +9,11 @@ DATABASES = {
 }
 SITE_ID = 3
 SECRET_KEY = 't@0=8$(m3+gcpf#a+z2&$=q+po--_&5n4#c8o%!s-5h$w)x2m3'
+
+# disable cache for dev and CI
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache'
+    }
+}
+
