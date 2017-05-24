38a24acc0099865cebddc31c2d3d475e8d2df3b0
Jirka Schaefer <jirka.schaefer@kkd-partners.com>
2017-05-24 08:26:11 +0200

    enable backup skip in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,11 +10,26 @@ source .ve/bin/activate
 git pull --no-edit && pip install -r requirements.txt | grep -v "Requirement already satisfied" || true
 
 echo "Creating fresh DB and media backups..."
+
+skip_backup=false
+while getopts ":a" opt; do
+  case $opt in
+    a)
+      skip_backup=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
 # needs to be run from site dir
-cd site
-./manage.py dbbackup
-./manage.py mediabackup
-cd ..
+if [ "$skip_backup" = true ] ; then
+  cd site
+  ./manage.py dbbackup
+  ./manage.py mediabackup
+  cd ..
+fi
 
 python ./scripts/minify_static.py && ./site/manage.py migrate && ./site/manage.py collectstatic --noinput
 
